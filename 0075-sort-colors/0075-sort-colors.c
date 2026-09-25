203d9b394e9d50f23322c89d670982a54d4ed537
VAIBHAV TYAGI <vaibhav.tyagi.cse.2025@miet.ac.in>
2026-09-25 06:24:01 +0530

    Time: 0 ms (100%), Space: 10.5 MB (74.59%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.c
@@ -0,0 +1,31 @@
+void sortColors(int* nums, int numsSize) {
+
+    int low = 0;
+    int mid = 0;
+    int high = numsSize - 1;
+
+    while (mid <= high) {
+
+        if (nums[mid] == 0) {
+            // Put 0 on the left
+            int temp = nums[low];
+            nums[low] = nums[mid];
+            nums[mid] = temp;
+
+            low++;
+            mid++;
+        }
+        else if (nums[mid] == 1) {
+            // 1 is already in the correct middle section
+            mid++;
+        }
+        else {
+            // Put 2 on the right
+            int temp = nums[mid];
+            nums[mid] = nums[high];
+            nums[high] = temp;
+
+            high--;
+        }
+    }
+}
