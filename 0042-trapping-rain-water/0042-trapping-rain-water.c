b6c654277261ef6f21932ca8e155464ae7612064
VAIBHAV TYAGI <vaibhav.tyagi.cse.2025@miet.ac.in>
2026-09-25 06:20:27 +0530

    Time: 0 ms (100%), Space: 10.5 MB (11.39%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.c
@@ -0,0 +1,39 @@
+int trap(int* height, int heightSize) {
+
+    if (heightSize <= 2)
+        return 0;
+
+    int left = 0;
+    int right = heightSize - 1;
+
+    int leftMax = 0;
+    int rightMax = 0;
+
+    int water = 0;
+
+    while (left < right) {
+
+        if (height[left] <= height[right]) {
+
+            if (height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                water += leftMax - height[left];
+            }
+
+            left++;
+
+        } else {
+
+            if (height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                water += rightMax - height[right];
+            }
+
+            right--;
+        }
+    }
+
+    return water;
+}
